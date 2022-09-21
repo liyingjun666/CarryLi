a5d145791a8859f7f7287699caad10d27dedb044
liyingjun666 <1324090676@qq.com>
2022-09-21 13:53:08 +0800

    20220921

---
+++ liyingjun_testng_01/src/main/java/com/liyingjun/LiyingjunTestng01Application.java
@@ -0,0 +1,13 @@
+package com.liyingjun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiyingjunTestng01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiyingjunTestng01Application.class, args);
+    }
+
+}
